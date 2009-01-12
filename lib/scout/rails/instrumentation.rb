bbf3b86f8b788bb94c092bf80ea43ce59ade3040
Matt Todd <chiology@gmail.com>
2009-01-12 16:08:27 -0500

    Whitespace fix.

--- lib/scout/rails/instrumentation.rb
+++ lib/scout/rails/instrumentation.rb
@@ -16,7 +16,7 @@ class ActionController::Base
       runtimes[:db] = ActiveRecord::Base.connection.reset_runtime + (@db_rt_before_render || 0.0) + (@db_rt_after_render || 0.0)
       runtimes[:view] = @rendering_runtime || @view_runtime
       runtimes[:total] = response.headers["X-Runtime"].to_f # runtimes[:db] + runtimes[:view] + time_in_controller + time_in_framework
-    
+      
       # @view_runtime is 2.3+ and is a good indication of the change from seconds to milliseconds
       Scout.report(runtimes, params, response, :in_seconds => @view_runtime.nil?)
     end
