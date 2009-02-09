072a9400dd3f60fd261d8df0af6827b395b1c6b5
Matt Todd <chiology@gmail.com>
2009-02-09 14:54:09 -0500

    Loads the plugin only when in production environment by default, and test helper loads it for testing.

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
-require 'scout/rails'
-Scout.start!
-ActionController::Base.class_eval do
-  alias_method_chain :perform_action, :instrumentation
+if RAILS_ENV == "production"
+  require 'scout/rails'
+  Scout.start!
+  ActionController::Base.class_eval do
+    alias_method_chain :perform_action, :instrumentation
+  end
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
+    alias_method_chain :log, :instrumentation
+  end
+  puts "** Scout Instrumentation Loaded"
 end
-ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-  alias_method_chain :log, :instrumentation
-end
-puts "** Scout Instrumentation Loaded"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,14 +6,14 @@ require 'mocha'
 
 ### Inject behavior into Rails (same as init.rb)
 
-# require 'scout/rails'
-# # Scout.start!
-# ActionController::Base.class_eval do
-#   alias_method_chain :perform_action, :instrumentation
-# end
-# ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-#   alias_method_chain :log, :instrumentation
-# end
+require 'scout/rails'
+Scout.start!
+ActionController::Base.class_eval do
+  alias_method_chain :perform_action, :instrumentation
+end
+ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
+  alias_method_chain :log, :instrumentation
+end
 
 ### Mocking methods
 
