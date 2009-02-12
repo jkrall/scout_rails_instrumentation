55c9c739dbce001e0d1b4d821e73d6c833395e68
Matt Todd <chiology@gmail.com>
2009-02-12 11:39:06 -0500

    Cleaned up the query explaining.

--- lib/scout/rails/instrumentation.rb
+++ lib/scout/rails/instrumentation.rb
@@ -37,14 +37,14 @@ class ActiveRecord::ConnectionAdapters::AbstractAdapter
     
     # temporary
     explained = nil
-    ms=Scout.seconds_to_ms(Time.now - start_time)
+    ms = Scout.seconds_to_ms(Time.now - start_time)
+    
     if sql =~ /^SELECT /i &&  ms > 50
       explained_sql = "EXPLAIN #{sql}"
       explained = execute(explained_sql).fetch_hash
     end
     unless Scout.queries.nil?
-      # Scout.queries << [ms, sql, start_time, explained ]
-      Scout.queries << [Scout.seconds_to_ms(Time.now - start_time),sql,start_time,explained]
+      Scout.queries << [ms, sql, start_time, explained]
     end
     
     results
