9531137c07acf54c53f301975f80e8f3399b92d8
Andre Lewis <andre@highgroove.com>
2009-02-19 22:07:11 -0800

    improved and reorganized readme and introductory text

---
+++ README.txt
@@ -0,0 +1,12 @@
+This plugin works together with http://scoutapp.com to provide insight into the health and performance 
+of your Rails application while it runs in production.
+
+It captures metrics silently in the background, imposing negligable overhead itself. 
+
+Scoutapp.com uses the gathered information to give you a heads-up when something changes on your app
+(for example, if a certain controller action slows down a lot relative to the same day last week), or if
+it looks like your database queries are having problems (for example, if a query is slow because you're missing an index.
+
+Learn more at http://scoutapp.com.
+
+(also see welcome.txt)
--- install.rb
+++ install.rb
@@ -1,22 +1,18 @@
 # This file is executed when the plugin is installed. It sets up the configuration file 
-# and provides brief instructions for 
+# and provides next steps to put the plugin to use.
 
-# template  
+# template 
+config_file_name='scout_config.yml'
 template_path=File.join(File.dirname(__FILE__), 'assets/scout_config_template')
-path=File.expand_path(File.join(File.dirname(__FILE__), 'scout_config.yml'))
+path=File.expand_path(File.join(File.dirname(__FILE__), config_file_name))
 
 if File.exists?(path)
-  puts "You already have a configuration file at #{path}. We've left it as-is. This is normal if you've re-installed the plugin. However, please check #{template_path} to see if anything has changed since your config file was created."
+  puts "You already have a configuration file at #{path}. We've left it as-is. This is normal if you've re-installed the plugin. "+
+       "However, please check #{template_path} to see if anything has changed since your config file was created."
 else
   File.open(path, "w") do |f|
     f.puts IO.read(template_path)
-    puts <<-EOS 
-** Welcome to Scout Rails instrumentation! **    
-
-1. You need to have an account at http://scoutapp.com to use this plugin 
-   (if you don't have an account yet, you can leave this plugin in place; it won't hurt anything)
-2. You need to set your Rails Instrumentation plugin id in the configuration file: #{path}.
-   Get your Rails Instrumentation plugin id from your account at http://scoutapp.com
-    EOS
-  end  
+  end   
 end
+
+puts File.read(File.join(File.dirname(__FILE__), 'welcome.txt')).gsub(config_file_name, path)
