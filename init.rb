d743fb44f1cbce00d48a06c3106ab158ad2a9280
Matt Todd <chiology@gmail.com>
2009-02-22 01:46:53 -0500

    Removed the explicit environment conditions from plugin init.

--- init.rb
+++ init.rb
@@ -1,11 +1,9 @@
-if RAILS_ENV == "production" || RAILS_ENV == "development"
-  require 'scout/rails'
-  Scout.start! do
-    ActionController::Base.class_eval do
-      alias_method_chain :perform_action, :instrumentation
-    end
-    ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-      alias_method_chain :log, :instrumentation
-    end
+require 'scout/rails'
+Scout.start! do
+  ActionController::Base.class_eval do
+    alias_method_chain :perform_action, :instrumentation
+  end
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
+    alias_method_chain :log, :instrumentation
   end
 end
