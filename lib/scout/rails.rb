4535a4862948ed5cf614577be385b791fa994be0
Joshua Krall <josh@transfs.com>
2009-05-03 17:32:28 -0500

    Fixed problems with RAILS_GEM_VERSION if rails is frozen, with 2-3-stable branch

--- lib/scout/rails.rb
+++ lib/scout/rails.rb
@@ -2,4 +2,4 @@ require File.join(File.dirname(__FILE__), '..', 'scout')
 require File.join(File.dirname(__FILE__), 'rails', 'benchmark')
 require File.join(File.dirname(__FILE__), 'rails', 'instrumentation')
 require File.join(File.dirname(__FILE__), 'rails', 'explains')
-require File.join(File.dirname(__FILE__), 'rails', 'x-runtime_fix') if RAILS_GEM_VERSION < "2.0.0"
+require File.join(File.dirname(__FILE__), 'rails', 'x-runtime_fix') if Rails.version < "2.0.0"
--- lib/scout/rails/benchmark.rb
+++ lib/scout/rails/benchmark.rb
@@ -1,5 +1,5 @@
-class << Benchmark
-  if RAILS_GEM_VERSION < '2.3.0'
+module Benchmark
+  if Rails.version < '2.3.0'
     def ms
       1000 * realtime { yield }
     end
